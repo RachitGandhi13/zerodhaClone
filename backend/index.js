1745a80905dd9e2dd59a1b9d6b84c082ebd4d23b
Rachit Gandhi <rachitgandhi138@gmail.com>
2025-05-31 23:20:44 +0530

    Save local changes before rebase

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 
 const {HoldingsModel}=require("./model/HoldingsModel")
 const {PositionsModel}=require("./model/PositionsModel")
+const {OrdersModel}=require("./model/OrdersModel")
 
 
 const PORT = process.env.PORT || 3002;
@@ -190,6 +191,17 @@ app.get('/allHoldings',async(req,res)=>{
 app.get('/allPositions',async(req,res)=>{
     let allPositions= await PositionsModel.find({});
     res.json(allPositions);
+});
+
+app.post('/newOrder',async(req,res)=>{
+    let newOrder= new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+    newOrder.save();
+    res.send("order saved");
 });
 
 app.listen(PORT, ()=>{
